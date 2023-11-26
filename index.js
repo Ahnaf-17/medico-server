300920363eb8c626247683179a0d6ced3ed132f2
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-26 16:38:44 +0600

    camp collection created and made a get request for it

--- index.js
+++ index.js
@@ -25,14 +25,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const campCollection = client.db('medicoDB').collection('camps');
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    // camps 
+    app.get('/camps',async(req,res)=>{
+      const result = await campCollection.find().toArray();
+      res.send(result)
+    })
+    
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);

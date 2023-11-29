6fc06cb8a05630c4ee6be77d81a7f85735403ca7
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-30 04:47:02 +0600

    reg-camps api modified

--- index.js
+++ index.js
@@ -248,12 +248,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/reg-camps/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await regCampCollection.findOne(query)
-      res.send(result)
-    })
+    // app.get('/reg-camps/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await regCampCollection.findOne(query)
+    //   res.send(result)
+    // })
 
     // reviews 
     app.get('/reviews', async (req, res) => {
